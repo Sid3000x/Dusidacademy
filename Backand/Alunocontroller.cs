bcc075b7b1127d888c7946e94f60e25fd2e1d595
Dusidacadimy <sidneyjunior1936@>
2023-09-16 12:32:17 -0300

    fiz a criacao do endpoint createaluno

---
+++ Backand/Alunocontroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DusiacademyAPI.ENTITIES;
+namespace DusiacademyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        //  [HttpGet] //obter
+        //   [HttpPost] // Criar
+        //   [HttDelete] // Deletar
+        //   [HttpPut] //Editar
+
+        [HttpPost(Name = "CreateAluno")]
+        public ActionResult CreateAluno (ALUNO aluno)
+        {
+            return Ok();
+
+        }
+
+
+
+
+
+    }
+
+}
+
+    
---
+++ Backand/Dusiacademy/ENTITIES/ALUNO.cs
@@ -0,0 +1,19 @@
+﻿namespace DusiacademyAPI.ENTITIES
+{
+    public class ALUNO
+    {
+          public int Matrícula { get; set; }
+          public string Nome { get; set; }
+          public int Altura { get; set;}
+          public int Peso { get;}
+          public string Sexo { get; set;} 
+          public string Etinia { get; set;}    
+          public int Idade { get; set;} 
+          public string Raça { get; set;}  
+          public int Telefone { get; set;}  
+          public string Endereço { get; set;}    
+          public string Email { get; set;}   
+            
+
+    }
+}
