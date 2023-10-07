eebbe0dab42a38e0fc5250a971f72476d3eb9037
Sidney Grisolia Junior <sidneyjunior19362gmail.com>
2023-10-07 12:28:46 -0300

    comentario

--- frontEnd/academiaApp/src/app/app.module.ts
+++ frontEnd/academiaApp/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { CadastroAlunosComponent } from './components/pages/cadastro-alunos/cadastro-alunos.component';
 import { ConsultaAlunosComponent } from './components/pages/consulta-alunos/consulta-alunos.component';
 import { EdicaoAlunosComponent } from './components/pages/edicao-alunos/edicao-alunos.component';
+import { CadastroFuncionarioComponent } from './components/pages/cadastro-funcionario/cadastro-funcionario.component';
+import { AppRoutingModule } from './app.routing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,15 @@ import { EdicaoAlunosComponent } from './components/pages/edicao-alunos/edicao-a
     NavbarComponent,
     CadastroAlunosComponent,
     ConsultaAlunosComponent,
-    EdicaoAlunosComponent
+    EdicaoAlunosComponent,
+    CadastroFuncionarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontEnd/academiaApp/src/app/app.routing.ts
+++ frontEnd/academiaApp/src/app/app.routing.ts
@@ -1,24 +1,28 @@
-import { RouterModule, Routes } from "@angular/router";
-import { CadastroAlunosComponent } from "./components/pages/cadastro-alunos/cadastro-alunos.component";
-import { ConsultaAlunosComponent } from "./components/pages/consulta-alunos/consulta-alunos.component";
-import { EdicaoAlunosComponent } from "./components/pages/edicao-alunos/edicao-alunos.component";
-import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CadastroAlunosComponent } from './components/pages/cadastro-alunos/cadastro-alunos.component';
+import { ConsultaAlunosComponent } from './components/pages/consulta-alunos/consulta-alunos.component';
+import { EdicaoAlunosComponent } from './components/pages/edicao-alunos/edicao-alunos.component';
+import { CadastroFuncionarioComponent } from './components/pages/cadastro-funcionario/cadastro-funcionario.component';
 
 
-// mapeamento das rotas do projeto
+//mapeamento das rotas do projeto
 const routes: Routes = [
-{ path: 'pages/cadastro-alunos', component: CadastroAlunosComponent },
-{ path: 'pages/consulta-alunos', component: ConsultaAlunosComponent },
-{ path: 'pages/edicao-alunos', component: EdicaoAlunosComponent },
-{ path: '', pathMatch: 'full', redirectTo:'pages/consulta-alunos' }
+  { path: 'pages/cadastro-alunos', component: CadastroAlunosComponent },
+  { path: 'pages/cadastro-funcionarios', component: CadastroFuncionarioComponent },
+  { path: 'pages/consulta-alunos', component: ConsultaAlunosComponent },
+  { path: 'pages/edicao-alunos/:id', component: EdicaoAlunosComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/pages/consulta-alunos' },
 ];
 
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-
 export class AppRoutingModule {}
+
+
+
+
 
